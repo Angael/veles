30649e94739a9fc6ee8957deb2fe970a95b7dd9b
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-01-22 10:16:01 +0100

    biome fixes and rule changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-
-import * as schema from './schema.ts';
 import { env } from 'env.ts';
+import { Pool } from 'pg';
+import * as schema from './schema.ts';
 
 const pool = new Pool({
 	connectionString: env.DATABASE_URL,
