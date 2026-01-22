1e7899b416c3323d437de43752e604d8aed90dee
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-01-22 10:56:47 +0100

    Add TypeScript check to GitHub Actions and update package.json scripts. Include bad check.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,6 +18,9 @@ export default function Header() {
 		Record<string, boolean>
 	>({});
 
+	const badVar: string = 123;
+	console.log({ badVar });
+
 	return (
 		<>
 			<header className='p-4 flex items-center bg-gray-800 text-white shadow-lg'>
