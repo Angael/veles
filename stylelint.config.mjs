1c19e7a7de6b83d4ecb950ce015323de5bf40076
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-06-07 12:52:29 +0200

    feat: add stylelint rules for improved CSS linting

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('stylelint').Config} */
 export default {
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': true,
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'custom-property-no-missing-var-function': true,
+    'declaration-block-no-duplicate-properties': true,
+    'declaration-block-no-shorthand-property-overrides': true,
+    'function-calc-no-unspaced-operator': true,
+    'media-query-no-invalid': true,
+    'no-duplicate-selectors': true,
+    'no-invalid-double-slash-comments': true,
+    'property-no-unknown': true,
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-no-unknown': true,
+    'unit-no-unknown': true,
+  },
 };
