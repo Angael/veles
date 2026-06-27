267c827fe2c823f5fcfe9c8c9c3f5737f92fef49
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-06-27 13:18:57 +0200

    slice at request

--- src/pages/recipes/recipes.api.ts
+++ src/pages/recipes/recipes.api.ts
@@ -51,7 +51,7 @@ export const getRecipeLibrary = createServerFn({ method: 'GET' })
         (recipe): RecipeLibraryItem => ({
           carbs: recipe.carbs,
           createdAt: recipe.createdAt.toISOString(),
-          description: recipe.description,
+          description: recipe.description.slice(0, 280),
           fats: recipe.fats,
           id: recipe.id,
           images: imagesByRecipeId.get(recipe.id) ?? [],
