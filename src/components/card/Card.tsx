4a00e832b9232ea5352f1a3d95efcb13a819ba78
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-07-13 12:19:24 +0200

    Improve shared error UI

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,10 +2,25 @@ import clsx from 'clsx';
 import type { ComponentPropsWithoutRef } from 'react';
 import css from './Card.module.css';
 
+export type CardVariant = 'default' | 'primary' | 'danger';
+
 type CardProps = ComponentPropsWithoutRef<'div'> & {
   as?: 'article' | 'aside' | 'div' | 'section';
+  shadow?: boolean;
+  variant?: CardVariant;
 };
 
-export function Card({ as: Component = 'div', className, ...props }: CardProps) {
-  return <Component className={clsx(css.card, className)} {...props} />;
+export function Card({
+  as: Component = 'div',
+  className,
+  shadow = true,
+  variant = 'default',
+  ...props
+}: CardProps) {
+  return (
+    <Component
+      className={clsx(css.card, css[variant], !shadow && css.noShadow, className)}
+      {...props}
+    />
+  );
 }
