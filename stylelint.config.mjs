e2e78d3255d682fb5f20da7281e16125b098acfb
Krzysztof Widacki <krzysztofwidacki@gmail.com>
2026-08-30 16:57:31 +0200

    oxlint ruleset 1

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('stylelint').Config} */
 export default {
+  extends: ['stylelint-config-recommended'],
   rules: {
     'at-rule-no-unknown': [
       true,
@@ -16,6 +17,7 @@ export default {
     'media-query-no-invalid': true,
     'no-duplicate-selectors': true,
     'no-invalid-double-slash-comments': true,
+    'no-descending-specificity': null,
     'property-no-unknown': true,
     'rule-empty-line-before': ['always', { except: ['first-nested'], ignore: ['after-comment'] }],
     'selector-pseudo-class-no-unknown': true,
